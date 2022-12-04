9ee43212ff7135866c082e4b7f176af3928ead2f
Claudio <leclaudiohenrique@gmail.com>
2022-12-04 14:53:16 -0300

    Att

--- src/main/java/com/redes/redes/Services/DetalheUsuarioServiceImpl.java
+++ src/main/java/com/redes/redes/Services/DetalheUsuarioServiceImpl.java
@@ -3,7 +3,6 @@ package com.redes.redes.Services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
